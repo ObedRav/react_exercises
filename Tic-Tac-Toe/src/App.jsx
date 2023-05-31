c6e9bf16c4348fc7b967d8670cd754b3ab18a190
Obed Rayo <obedrayo2356@gmail.com>
2023-05-30 20:00:15 -0500

    Adding Tic Tac Toe

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,129 @@
+import { useState } from 'react'
+import './App.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINS_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+  
+  const checkWinner = (boardCheck) => {
+    for (const comb in WINS_COMBOS) {
+      const [a, b, c] = comb
+
+      if (boardCheck[a] && boardCheck[a] === boardCheck[b] && boardCheck[b] === boardCheck[c]) {
+        return boardCheck[a]
+      }
+
+      return null
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return // If there something
+
+    // Changing the board
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // Changing the turn
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //Check if there's a winner
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1> Tic Tac Toe </h1>
+      <buttom onClick={resetGame}>Reset Game</buttom>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>  
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false ? 'Empate' : 'Gano'
+                }
+              </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <buttom onClick={resetGame}>Start again</buttom>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+      <section></section>
+    </main>
+  )
+}
+
+export default App
